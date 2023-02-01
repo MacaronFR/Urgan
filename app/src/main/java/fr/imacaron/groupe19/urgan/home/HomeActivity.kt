a1fc1c8f41a4f4349ff346c2b6c18b3fd170bb70
Denis Turbiez <denisft77@gmail.com>
2023-02-01 22:56:48 +0100

    better back press

--- app/src/main/java/fr/imacaron/groupe19/urgan/home/HomeActivity.kt
+++ app/src/main/java/fr/imacaron/groupe19/urgan/home/HomeActivity.kt
@@ -6,11 +6,17 @@ import androidx.appcompat.app.AppCompatActivity
 import fr.imacaron.groupe19.urgan.backend.firebase.FirebaseAPIManager
 import fr.imacaron.groupe19.urgan.data.User
 import fr.imacaron.groupe19.urgan.databinding.ActivityHomeBinding
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class HomeActivity: AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
 
     lateinit var user: User
+
+    private var confirm: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,10 +25,20 @@ class HomeActivity: AppCompatActivity() {
         setContentView(binding.root)
     }
 
-    override fun onBackPressed() {
-        FirebaseAPIManager.signOut()
-        Toast.makeText(this, "Log Out Successful", Toast.LENGTH_LONG).show()
-        this.finish()
+    @OptIn(DelicateCoroutinesApi::class)
+    override fun onBackPressed(){
+        if(confirm){
+            FirebaseAPIManager.signOut()
+            Toast.makeText(this, "Log Out Successful", Toast.LENGTH_LONG).show()
+            this.finish()
+        }else{
+            Toast.makeText(this, "Appuyer à nouveau pour vous déconnecter", Toast.LENGTH_SHORT).show()
+            confirm = true
+            GlobalScope.launch {
+                delay(5000)
+                confirm = false
+            }
+        }
     }
 
 }
