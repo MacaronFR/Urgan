4589c39561b4b118de7e68a5de1a840f31ed3581
Denis Turbiez <denisft77@gmail.com>
2023-02-02 11:10:48 +0100

    stars

--- app/src/main/java/fr/imacaron/groupe19/urgan/list/ReviewAdapter.kt
+++ app/src/main/java/fr/imacaron/groupe19/urgan/list/ReviewAdapter.kt
@@ -3,6 +3,7 @@ package fr.imacaron.groupe19.urgan.list
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
@@ -14,16 +15,27 @@ import fr.imacaron.groupe19.urgan.error.NetworkException
 import fr.imacaron.groupe19.urgan.error.h
 import fr.imacaron.groupe19.urgan.views.UnderLineTextView
 import kotlinx.coroutines.*
+import java.util.Random
 
 class ReviewAdapter(private val dataSet: List<Review>, private val fragment: Fragment): RecyclerView.Adapter<ReviewAdapter.ViewHolder>() {
 
     class ViewHolder(view: View): RecyclerView.ViewHolder(view){
         val review: TextView
         val name: UnderLineTextView
+        val star1: ImageView
+        val star2: ImageView
+        val star3: ImageView
+        val star4: ImageView
+        val star5: ImageView
 
         init {
             review = view.findViewById(R.id.review)
             name = view.findViewById(R.id.name)
+            star1 = view.findViewById(R.id.star1)
+            star2 = view.findViewById(R.id.star2)
+            star3 = view.findViewById(R.id.star3)
+            star4 = view.findViewById(R.id.star4)
+            star5 = view.findViewById(R.id.star5)
         }
     }
 
@@ -38,6 +50,7 @@ class ReviewAdapter(private val dataSet: List<Review>, private val fragment: Fra
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         with(dataSet[position]) review@{
             holder.review.text = this.review
+            stars(holder, this.like)
             GlobalScope.launch {
                 withContext(Dispatchers.IO + h){
                     val u = try {
@@ -52,6 +65,28 @@ class ReviewAdapter(private val dataSet: List<Review>, private val fragment: Fra
                         holder.name.text = u.response.players[0].name
                     }
                 }
+            }
+        }
+    }
+
+    private fun stars(holder: ViewHolder, like: Boolean){
+        val delta = Random().nextInt(3)
+        if(like){
+            holder.star1.setImageResource(R.drawable.star_full)
+            holder.star2.setImageResource(R.drawable.star_full)
+            holder.star3.setImageResource(R.drawable.star_full)
+            if(delta >= 1){
+                holder.star4.setImageResource(R.drawable.star_full)
+            }
+            if(delta >= 2){
+                holder.star5.setImageResource(R.drawable.star_full)
+            }
+        }else{
+            if(delta >= 1){
+                holder.star1.setImageResource(R.drawable.star_full)
+            }
+            if(delta >= 2){
+                holder.star2.setImageResource(R.drawable.star_full)
             }
         }
     }
